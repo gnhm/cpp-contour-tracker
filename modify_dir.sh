9a24f9a2d5190e62df15547ee46b3bab9eddaeef
Jamie <69422343+jsm8989@users.noreply.github.com>
2022-02-11 18:31:45 +0100

    Update modify_dir.sh

--- modify_dir.sh
+++ modify_dir.sh
@@ -1,6 +1,7 @@
 # to pull from remote repo, delete the old bash executable, and compile the new imported cpp file
-rm track_movie # if it exists
+rm track_movie 
+rm track_contours
 git pull
 
 g++ -o track_movie track_movie.cpp
-
+g++ -o track_contours track_contours.cpp
