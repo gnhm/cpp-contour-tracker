e23fe98d6c318dc1c8f67f3428e74c105ca2ee47
Jamie <69422343+jsm8989@users.noreply.github.com>
2022-02-11 18:08:39 +0100

    create for debugging

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <temika_header.h>
+#include <get_movie_frame.h>
+
+
+int main (int argc, char** argv) {
+  
+  FILE moviefile = argv[1]
+  get_frame(moviefile, struct camera_frame_struct *temika_frame) // (FILE *moviefile, struct camera_frame_struct *temika_frame) 
+  
+  
+  
+}
